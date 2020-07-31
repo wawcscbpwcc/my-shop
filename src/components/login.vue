419ff0d3244fa1d9f516a08e961f89555d5be09c
wenawen <257609247@qq.com>
2020-07-31 08:57:11 +0800

    完成了登录功能

---
+++ src/components/login.vue
@@ -0,0 +1,158 @@
+<!--  -->
+<template>
+  <div class="login_container">
+    <div class="login_box">
+      <!-- 头像区 -->
+      <div class="avatar_box">
+        <img src="../assets/logo.png" alt />
+      </div>
+
+      <!-- 登录表单区 -->
+      <el-form
+        label-width="80px"
+        class="login_form"
+        :model="loginForm"
+        :rules="loginFormRules"
+        ref="loginFormRef"
+      >
+        <!-- 用户名区域 -->
+        <el-form-item label="用户名" prop="username">
+          <el-input prefix-icon="el-icon-user-solid" v-model="loginForm.username"></el-input>
+        </el-form-item>
+
+        <!-- 密码区域 -->
+        <el-form-item label="密码" prop="password">
+          <el-input prefix-icon="el-icon-lock" v-model="loginForm.password" type="password"></el-input>
+        </el-form-item>
+
+        <!-- 按钮区域 -->
+        <el-form-item class="btns">
+          <el-button type="primary" @click="login">登录</el-button>
+          <el-button type="info" @click="resetLoginForm">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+
+<script>
+//这里可以导入其他文件（比如：组件，工具js，第三方插件js，json文件，图片文件等等）
+//例如：import 《组件名称》 from '《组件路径》';
+
+export default {
+  //import引入的组件需要注入到对象中才能使用
+  components: {},
+  data() {
+    //这里存放数据
+    return {
+      // 登录表单数据绑定
+      loginForm: {
+        username: "",
+        password: "",
+      },
+      // 表单验证规则
+      loginFormRules: {
+        // 验证用户名
+        username: [
+          { required: true, message: "请输入用户名", trigger: "blur" },
+          {
+            min: 4,
+            max: 16,
+            message: "用户名长度必须在4到16位之间",
+            trigger: "blur",
+          },
+        ],
+        // 验证密码
+        password: [
+          { required: true, message: "请输入密码", trigger: "blur" },
+          {
+            min: 8,
+            max: 16,
+            message: "用户名长度必须在8到16位之间",
+            trigger: "blur",
+          },
+        ],
+      },
+    };
+  },
+  //监听属性 类似于data概念
+  computed: {},
+  //监控data中的数据变化
+  watch: {},
+  //方法集合
+  methods: {
+    //   点击重置登录表单
+    resetLoginForm() {
+      this.$refs.loginFormRef.resetFields();
+    },
+    login() {
+      this.$refs.loginFormRef.validate(async (valid) => {
+        if (!valid) return;
+        const { data: res } = await this.$http.post("login", this.loginForm);
+        if (res.meta.status !== 200) return this.$message.error("登录失败");
+        this.$message.success("登录成功");
+
+        window.sessionStorage.setItem('token',res.data.token)
+        this.$router.push('/home')
+      });
+    },
+  },
+  //生命周期 - 创建完成（可以访问当前this实例）
+  created() {},
+  //生命周期 - 挂载完成（可以访问DOM元素）
+  mounted() {},
+  beforeCreate() {}, //生命周期 - 创建之前
+  beforeMount() {}, //生命周期 - 挂载之前
+  beforeUpdate() {}, //生命周期 - 更新之前
+  updated() {}, //生命周期 - 更新之后
+  beforeDestroy() {}, //生命周期 - 销毁之前
+  destroyed() {}, //生命周期 - 销毁完成
+  activated() {}, //如果页面有keep-alive缓存功能，这个函数会触发
+};
+</script>
+<style lang='less' scoped>
+.login_container {
+  background-color: #2b4b6b;
+  height: 100%;
+  .login_box {
+    width: 450px;
+    height: 300px;
+    background-color: #fff;
+    border-radius: 3px;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    .avatar_box {
+      width: 130px;
+      height: 130px;
+      border: 1px solid #eee;
+      border-radius: 50%;
+      padding: 10px;
+      box-shadow: 0 0 10px #ddd;
+      position: absolute;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      background-color: #fff;
+      img {
+        width: 100%;
+        height: 100%;
+        border-radius: 50%;
+        background-color: #eee;
+      }
+    }
+  }
+}
+
+.login_form {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  padding: 0 20px;
+  box-sizing: border-box;
+}
+.btns {
+  display: flex;
+  justify-content: flex-end;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import './plugins/element.js'
+import axios from 'axios'
 
+Vue.prototype.$http = axios
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+// 导入全局样式表
+import "./assets/css/global.css"
 Vue.config.productionTip = false
 
 new Vue({
