7c67e8efb1854fa9fd91d1f4389545417df4249e
wenawen <257609247@qq.com>
2020-08-04 11:24:52 +0800

    完成了分类功能的开发

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -34,7 +34,8 @@ import {
   Tag,
   Tree,
   Select,
-  Option
+  Option,
+  Cascader
 } from 'element-ui'
 
 
@@ -65,5 +66,6 @@ Vue.use(Tag)
 Vue.use(Tree)
 Vue.use(Select)
 Vue.use(Option)
+Vue.use(Cascader)
 Vue.prototype.$message = Message
 Vue.prototype.$confirm = MessageBox.confirm
